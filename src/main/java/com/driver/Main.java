95665658454bb3deff6d6fab99d3841d03acb1f6
Mohammadkasaf <kaashifchishti611@gmail.com>
2024-05-04 13:54:47 +0530

    commit message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,36 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
 
-    static class Product {
+        // Task 3
+        int result1 = p.product(2, 3);
 
+
+        // Task 4
+        int result2 = p.product(2, 3, 4);
+
+
+        // Task 5
+        double result3 = p.product(2.5, 3.5);
+
+    }
+
+    public static class Product {
+        // Task 3
         public int product(int x, int y) {
             return x * y;
         }
 
+        // Task 4
         public int product(int x, int y, int z) {
             return x * y * z;
         }
 
+        // Task 5
         public double product(double x, double y) {
             return x * y;
         }
-    }
-
-    public static void main(String args[]) {
-
-        Product p = new Product();
-        System.out.println("Product of 1 and 2: " + p.product(1, 2));
-        System.out.println("Product of 2, 3, and 4: " + p.product(2, 3, 4));
-        System.out.println("Product of 2.3 and 5.6: " + p.product(2.3d, 5.6d));
     }
 }
